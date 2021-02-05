92adff0f82a4f1f98df6d6ce03f6d11004d3e542
paoloespiritu_cap <paoloespiritu@outlook.com>
2021-02-05 22:03:35 +0000

    step 1 - 7

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// task 1
 sap.ui.define([
 
 ], function () {
@@ -6,4 +7,31 @@ sap.ui.define([
 	/* eslint-disable no-alert */
 	alert("UI5 is ready");
 	/* eslint-enable no-alert */
+});
+
+// task 2
+sap.ui.define([
+	"sap/m/Text"
+
+], function (Text) {
+	"use strict";
+
+	new Text({
+		text: "Hello World: From task 2 in the JS file!"
+	}).placeAt("content");
+
+});
+
+// task 3
+sap.ui.define([
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
+	"use strict";
+
+	XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
+
 });
