ac73c857f4e654f9b67aa5c101b2b53292552aa7
paoloespiritu_cap <paoloespiritu@outlook.com>
2021-05-04 12:43:26 +0100

    step10 walkthrough

--- webapp/Component.js
+++ webapp/Component.js
@@ -5,14 +5,17 @@ sap.ui.define([
  ], function (UIComponent, JSONModel, ResourceModel) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-       metadata : {
-          "rootView": {
-             "viewName": "sap.ui.demo.walkthrough.view.App",
-             "type": "XML",
-             "async": true,
-             "id": "app"
-          }
-       },
+      //  metadata : {
+      //     "rootView": {
+      //        "viewName": "sap.ui.demo.walkthrough.view.App",
+      //        "type": "XML",
+      //        "async": true,
+      //        "id": "app"
+      //     }
+      //  },
+      metadata : {
+         manifest: "json"
+      },
        init : function () {
           // call the init function of the parent
           UIComponent.prototype.init.apply(this, arguments);
