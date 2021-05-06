bc744e902fe52b55421509d970423ab630ec9116
paoloespiritu_cap <paoloespiritu@outlook.com>
2021-05-06 14:16:59 +0100

    walkthrough step15 separate view

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -43,13 +43,13 @@ sap.ui.define([
 ], function (Controller, MessageToast) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onShowHello : function () {
-         // read msg from i18n model
-         var oBundle = this.getView().getModel("i18n").getResourceBundle();
-         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-         // show message
-         MessageToast.show(sMsg);
-      }
+      // onShowHello : function () {
+      //    // read msg from i18n model
+      //    var oBundle = this.getView().getModel("i18n").getResourceBundle();
+      //    var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+      //    var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+      //    // show message
+      //    MessageToast.show(sMsg);
+      // }
    });
 });
